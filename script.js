230308f7b7ff2290e865c237e6e3999682842b73
mohamad-azhar <mohamad.sheikh.aleshra@student.ehb.be>
2026-07-24 21:00:20 +0200

    speelkaarten met JavaScript toegevoegd

--- script.js
+++ script.js
@@ -7,10 +7,50 @@ const restartButton = document.querySelector("#restart-button");
 
 console.log("Memory Game is correct geladen:)");
 
+const cardSymbols = [
+    "🍎",
+    "🍌",
+    "🍇",
+    "🍓",
+    "🍎",
+    "🍌",
+    "🍇",
+    "🍓"
+];
 
-restartButton.addEventListener("click", function(){
+function shuffleCards(cards){
+    return cards.sort(function() {
+        return Math.random() -0.5;
+    });
+}
+
+function createGameBoard() {
+    gameBoard.innerHTML = "";
+
+    const shuffledCards = shuffleCards([...cardSymbols]);
+
+    shuffledCards.forEach(function (symbol) {
+        const card = document.createElement("button");
+
+        card.classList.add("card");
+        card.setAttribute("type", "button");
+        card.dataset.symbol = symbol;
+        card.textContent = "?";
+
+        gameBoard.appendChild(card);
+    });
+}
+
+function restartGame() {
     movesElement.textContent = "0";
-    timerElement.textContent = "0:00";
+    timerElement.textContent = "0";
 
-    console.log("Het spel werd opnieuw gestart!");
-});
+    createGameBoard();
+}
+
+
+
+restartButton.addEventListener("click", restartGame);
+
+
+createGameBoard();
